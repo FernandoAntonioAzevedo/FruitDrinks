d5122f64e0e17cec813601f6f745309b355745ab
FernandoAntonioAzevedo <beratechsolutions@gmail.com>
2026-05-30 19:39:15 -0300

    finalizando botão e iniciando ajustes na rolagem do cabeçário

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,15 @@
 /* Menu hidden */
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+	const header = document.getElementById('header');
+
+	this.scrollY >= 50 
+	? header.classList.add('bg-header') 
+	: header.classList.remove('bg-header');
+};
+
+window.addEventListener('scroll', scrollHeader);
 
 /*=============== HOME HEIGHT ===============*/
 
