04d0a3336aba0dc1de24d2dc307892534af5829d
FernandoAntonioAzevedo <beratechsolutions@gmail.com>
2026-06-08 17:32:03 -0300

    finalizando os breakpoints da responsividade e iniciando as animações GSAP

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,17 @@
 /*=============== MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+navToggle = document.getElementById('nav-toggle'),
+navClose = document.getElementById('nav-close');
 
 /* Menu show */
+navToggle.addEventListener('click', () => {
+	navMenu.classList.add('show-menu');
+});
 
 /* Menu hidden */
+navClose.addEventListener('click', () => {
+	navMenu.classList.remove('show-menu');
+});
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 const scrollHeader = () => {
